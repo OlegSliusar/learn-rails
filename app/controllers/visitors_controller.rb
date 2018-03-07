7452ab3f819f80c98d86c0a237d337619dc6aa8b
Oleh Sliusar <olehsliusar@gmail.com>
2018-03-07 18:37:42 +0200

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,12 +2,6 @@ class VisitorsController < ApplicationController
 
   def new
     @owner = Owner.new
-    flash.now[:notice] = 'Welcome!'
-    flash.now[:alert] = 'My birthday is soon.'
-    
-    flash.each do |key, value|
-      puts '' + value + ''
-    end
   end
 
 end
